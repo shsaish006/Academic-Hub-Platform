98f24413b4764411867b96f38ba4cd7d063e6511
Saish006 <shivamsai006@gmail.com>
2026-02-20 10:40:29 +0530

    Initial commit

---
+++ classroom-frontend-main/src/App.tsx
@@ -0,0 +1,95 @@
+import { GitHubBanner, Refine  } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import routerProvider, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router";
+import {BrowserRouter, Outlet, Route, Routes} from "react-router";
+import "./App.css";
+import { Toaster } from "./components/refine-ui/notification/toaster";
+import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
+import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
+import Dashboard from "@/pages/dashboard.tsx";
+import {BookOpen, GraduationCap, Home} from "lucide-react";
+import {Layout} from "@/components/refine-ui/layout/layout.tsx";
+import SubjectsList from "@/pages/subjects/list.tsx";
+import SubjectsCreate from "@/pages/subjects/create.tsx";
+
+import ClassesList from "@/pages/classes/list.tsx";
+import ClassesCreate from "@/pages/classes/create.tsx";
+import ClassesShow from "@/pages/classes/show.tsx";
+
+import {dataProvider} from "@/providers/data.ts";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RefineKbarProvider>
+        <ThemeProvider>
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={dataProvider}
+              notificationProvider={useNotificationProvider()}
+              routerProvider={routerProvider}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                projectId: "jtNqZy-1VTAjv-OXwtBn",
+              }}
+              resources={[
+                  {
+                      name: 'dashboard',
+                      list: '/',
+                      meta: { label: 'Home', icon: <Home />}
+                  },
+                  {
+                      name: 'subjects',
+                      list: '/subjects',
+                      create: '/subjects/create',
+                      meta: { label: 'Subjects', icon: <BookOpen />}
+                  },
+                  {
+                      name: 'classes',
+                      list: '/classes',
+                      create: '/classes/create',
+                      show: '/classes/show/:id',
+                      meta: { label: 'Classes', icon: <GraduationCap />}
+                  }
+              ]}
+            >
+              <Routes>
+                  <Route element={
+                      <Layout>
+                          <Outlet />
+                      </Layout>
+                  }>
+                    <Route path="/" element={<Dashboard />} />
+
+                      <Route path="subjects">
+                          <Route index element={<SubjectsList />} />
+                          <Route path="create" element={<SubjectsCreate />} />
+                      </Route>
+
+                      <Route path="classes">
+                          <Route index element={<ClassesList />} />
+                          <Route path="create" element={<ClassesCreate />} />
+                          <Route path="show/:id" element={<ClassesShow />} />
+                      </Route>
+                  </Route>
+              </Routes>
+              <Toaster />
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
+        </ThemeProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
